46ad4d37f29488203cbb9c1f9dbafa974b8aa918
Daniil Mosolov <dkuets@gmail.com>
2025-05-20 15:07:53 +0300

    feat(serviceProvider): add category and article inits

--- internal/app/service_provider.go
+++ internal/app/service_provider.go
@@ -10,8 +10,12 @@ import (
 	"github.com/nogavadu/articles-service/internal/config"
 	"github.com/nogavadu/articles-service/internal/config/env"
 	"github.com/nogavadu/articles-service/internal/repository"
+	articleRepo "github.com/nogavadu/articles-service/internal/repository/article"
+	categoryRepo "github.com/nogavadu/articles-service/internal/repository/category"
 	cropRepo "github.com/nogavadu/articles-service/internal/repository/crop"
 	"github.com/nogavadu/articles-service/internal/service"
+	articleServ "github.com/nogavadu/articles-service/internal/service/article"
+	categoryServ "github.com/nogavadu/articles-service/internal/service/category"
 	cropServ "github.com/nogavadu/articles-service/internal/service/crop"
 	"log/slog"
 	"os"
@@ -75,14 +79,14 @@ func (p *serviceProvider) Logger() *slog.Logger {
 
 func (p *serviceProvider) CropImpl(ctx context.Context) *crop.Implementation {
 	if p.cropImpl == nil {
-		p.cropImpl = crop.New(p.CropService(p.Logger(), ctx))
+		p.cropImpl = crop.New(p.CropService(ctx))
 	}
 	return p.cropImpl
 }
 
-func (p *serviceProvider) CropService(logger *slog.Logger, ctx context.Context) service.CropService {
+func (p *serviceProvider) CropService(ctx context.Context) service.CropService {
 	if p.cropService == nil {
-		p.cropService = cropServ.New(logger, p.CropRepository(ctx))
+		p.cropService = cropServ.New(p.Logger(), p.CropRepository(ctx))
 	}
 	return p.cropService
 }
@@ -92,6 +96,48 @@ func (p *serviceProvider) CropRepository(ctx context.Context) repository.CropRep
 		p.cropRepository = cropRepo.New(p.DBClient(ctx))
 	}
 	return p.cropRepository
+}
+
+func (p *serviceProvider) CategoryImpl(ctx context.Context) *category.Implementation {
+	if p.categoryImpl == nil {
+		p.categoryImpl = category.New(p.CategoryService(ctx))
+	}
+	return p.categoryImpl
+}
+
+func (p *serviceProvider) CategoryService(ctx context.Context) service.CategoryService {
+	if p.categoryService == nil {
+		p.categoryService = categoryServ.New(p.Logger(), p.CategoryRepository(ctx))
+	}
+	return p.categoryService
+}
+
+func (p *serviceProvider) CategoryRepository(ctx context.Context) repository.CategoryRepository {
+	if p.categoryRepository == nil {
+		p.categoryRepository = categoryRepo.New(p.DBClient(ctx))
+	}
+	return p.categoryRepository
+}
+
+func (p *serviceProvider) ArticleImpl(ctx context.Context) *article.Implementation {
+	if p.articlesImpl == nil {
+		p.articlesImpl = article.New(p.ArticleService(ctx))
+	}
+	return p.articlesImpl
+}
+
+func (p *serviceProvider) ArticleService(ctx context.Context) service.ArticleService {
+	if p.articleService == nil {
+		p.articleService = articleServ.New(p.Logger(), p.ArticleRepository(ctx))
+	}
+	return p.articleService
+}
+
+func (p *serviceProvider) ArticleRepository(ctx context.Context) repository.ArticleRepository {
+	if p.articleRepository == nil {
+		p.articleRepository = articleRepo.New(p.DBClient(ctx))
+	}
+	return p.articleRepository
 }
 
 func (p *serviceProvider) DBClient(ctx context.Context) *pgxpool.Pool {
